1448e00aaf4e937d938a3ebd2f995afd863d4ce7
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2015-10-15 13:58:54 +0100

    Added a multi-project test with dependencies.

--- src/functional/groovy/com/github/rodm/teamcity/AgentAndServerPluginFunctionalTest.groovy
+++ src/functional/groovy/com/github/rodm/teamcity/AgentAndServerPluginFunctionalTest.groovy
@@ -94,6 +94,93 @@ public class AgentAndServerPluginFunctionalTest {
         assertThat(entries, hasItem('server/server-lib.jar'))
     }
 
+
+    @Test
+    public void multiProjectPlugin() {
+        buildFile << """
+            buildscript {
+                dependencies {
+                    classpath files(${pluginClasspath})
+                }
+            }
+
+            subprojects {
+                apply plugin: 'java'
+            }
+            project(':agent') {
+                dependencies {
+                    compile project(':common')
+                    compile files("\$rootDir/lib/agent-lib.jar")
+                }
+            }
+            project(':server') {
+                dependencies {
+                    compile project(':common')
+                    compile files("\$rootDir/lib/server-lib.jar")
+                }
+            }
+
+            apply plugin: 'com.github.rodm.teamcity-agent'
+            apply plugin: 'com.github.rodm.teamcity-server'
+
+            dependencies {
+                agent project(':agent')
+                agent project(':common')
+                agent files("\$rootDir/lib/agent-lib.jar")
+                server project(':server')
+                server project(':common')
+                server files("\$rootDir/lib/server-lib.jar")
+            }
+
+            teamcity {
+                version = '8.1.5'
+                agent {
+                    descriptor {
+                    }
+                }
+                server {
+                    descriptor {
+                    }
+                }
+            }
+        """
+
+        File settingsFile = testProjectDir.newFile('settings.gradle')
+        settingsFile << """
+            rootProject.name = 'test-plugin'
+            include 'common'
+            include 'agent'
+            include 'server'
+        """
+
+        testProjectDir.newFolder('lib')
+        testProjectDir.newFile('lib/agent-lib.jar')
+        testProjectDir.newFile('lib/server-lib.jar')
+
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(testProjectDir.getRoot())
+                .withArguments("build")
+                .build();
+
+        assertEquals(result.task(":agentPlugin").getOutcome(), SUCCESS)
+        assertEquals(result.task(":serverPlugin").getOutcome(), SUCCESS)
+
+        ZipFile agentPluginFile = new ZipFile(new File(testProjectDir.root, 'build/distributions/test-plugin-agent.zip'))
+        List<String> agentEntries = agentPluginFile.entries().collect { it.name }
+        assertThat(agentEntries, hasItem('teamcity-plugin.xml'))
+        assertThat(agentEntries, hasItem('lib/common.jar'))
+        assertThat(agentEntries, hasItem('lib/agent.jar'))
+        assertThat(agentEntries, hasItem('lib/agent-lib.jar'))
+
+        ZipFile serverPluginFile = new ZipFile(new File(testProjectDir.root, 'build/distributions/test-plugin.zip'))
+        List<String> entries = serverPluginFile.entries().collect { it.name }
+        assertThat(entries, hasItem('teamcity-plugin.xml'))
+        assertThat(entries, hasItem('agent/test-plugin-agent.zip'))
+        assertThat(entries, hasItem('server/common.jar'))
+        assertThat(entries, hasItem('server/server.jar'))
+        assertThat(entries, hasItem('server/server-lib.jar'))
+    }
+
     @Test
     public void pluginsWithJavaFail() {
         buildFile << """
