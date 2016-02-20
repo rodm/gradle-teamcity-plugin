dbf03452c626b38c6633c160fb96641975c85141
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2016-02-20 22:55:40 +0300

    Add simple plugin for using TeamCity common-api
    It's useful for common modules shared between agent and server parts of plugin.
    New plugin named 'com.github.rodm.teamcity-common'.
    It just adds 'provided' configuration and 'org.jetbrains.teamcity:common-api:$version' dependency

--- samples/multi-project-plugin/common/src/main/java/example/common/ExampleBuildFeature.java
+++ samples/multi-project-plugin/common/src/main/java/example/common/ExampleBuildFeature.java
@@ -1,7 +1,14 @@
 
 package example.common;
 
+import jetbrains.buildServer.util.StringUtil;
+
 public class ExampleBuildFeature {
 
     public static final String BUILD_FEATURE_NAME = "example";
+
+    {
+        // Just simple usage of TeamCity Common API (shared between server and agent)
+        assert StringUtil.isTrue("true");
+    }
 }
