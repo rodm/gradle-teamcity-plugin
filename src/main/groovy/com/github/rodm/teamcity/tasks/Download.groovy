58ce40734f1dd810247bb1d1680ae84c53a20ea3
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2016-02-24 12:42:03 +0000

    Download task shows progress
     Fixes #3

--- src/main/groovy/com/github/rodm/teamcity/tasks/Download.groovy
+++ src/main/groovy/com/github/rodm/teamcity/tasks/Download.groovy
@@ -19,8 +19,14 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import org.gradle.logging.ProgressLogger
+import org.gradle.logging.ProgressLoggerFactory
+
+import javax.inject.Inject
 
 class Download extends DefaultTask {
+
+    private static final int BUFFER_SIZE = 10000;
 
     @Input
     String source
@@ -28,9 +34,51 @@ class Download extends DefaultTask {
     @OutputFile
     File target
 
+    @Inject
+    protected ProgressLoggerFactory getProgressLoggerFactory() {
+         throw new UnsupportedOperationException()
+    }
+
     @TaskAction
     void download() {
-        logger.info("Downloading TeamCity installer from {} to {}", getSource(), getTarget())
-        new URL(getSource()).withInputStream { is -> getTarget().withOutputStream { os -> os << is }}
+        logger.quiet("Downloading {}", getSource())
+
+        ProgressLogger progressLogger = getProgressLoggerFactory().newOperation(getClass())
+        progressLogger.setDescription("Download TeamCity installer")
+        progressLogger.setShortDescription(getName())
+
+        OutputStream os = null
+        InputStream is = null
+        try {
+            progressLogger.started()
+
+            os = new BufferedOutputStream(new FileOutputStream(getTarget()))
+            URLConnection conn = new URL(getSource()).openConnection()
+            is = conn.getInputStream()
+            int bytesRead
+            long totalBytesRead = 0
+            long loggedMb = 0
+            byte[] buffer = new byte[BUFFER_SIZE]
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+
+                totalBytesRead += bytesRead;
+                long processedMb = totalBytesRead / (1024 * 1024)
+                if (processedMb > loggedMb) {
+                    progressLogger.progress(String.format("%dMB downloaded", processedMb))
+                    loggedMb = processedMb
+                }
+            }
+        }
+        finally {
+            progressLogger.completed()
+
+            if (is != null) {
+                is.close();
+            }
+            if (os != null) {
+                os.close();
+            }
+        }
     }
 }
