4fd1ef958769ebaca1b6bc10a39e794c6491f64f
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2017-04-07 12:31:40 +0100

    Add example that builds 2 plugins and deploys them to an environment

---
+++ samples/multiple-plugins/plugin2/src/main/java/example/teamcity/Example2ServerPlugin.java
@@ -0,0 +1,27 @@
+package example.teamcity;
+
+import jetbrains.buildServer.serverSide.BuildServerAdapter;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import org.apache.log4j.Logger;
+
+public class Example2ServerPlugin extends BuildServerAdapter {
+
+    private static Logger logger = Logger.getLogger("jetbrains.buildServer.SERVER");
+
+    private final SBuildServer server;
+
+    private final PluginDescriptor descriptor;
+
+    public Example2ServerPlugin(SBuildServer server, PluginDescriptor descriptor) {
+        this.server = server;
+        this.descriptor = descriptor;
+    }
+
+    public void register() {
+        server.addListener(this);
+        String name = getClass().getSimpleName();
+        logger.info(String.format("%s: Plugin name: %s, version: %s", name, descriptor.getPluginName(), descriptor.getPluginVersion()));
+        logger.info(String.format("%s: Plugin parameter: param=%s", name, descriptor.getParameterValue("param")));
+    }
+}
