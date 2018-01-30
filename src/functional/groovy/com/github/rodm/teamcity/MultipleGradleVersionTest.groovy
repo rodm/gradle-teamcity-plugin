d66c5d31f128bb0085c019b870bee1e14d9aa8cc
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-01-30 15:41:41 +0000

    Update Gradle versions used for testing

--- src/functional/groovy/com/github/rodm/teamcity/MultipleGradleVersionTest.groovy
+++ src/functional/groovy/com/github/rodm/teamcity/MultipleGradleVersionTest.groovy
@@ -56,8 +56,8 @@ class MultipleGradleVersionTest {
     @Parameterized.Parameters(name = 'Gradle {0}')
     static List<String> data() {
         return [
-                '3.0', '3.1', '3.2', '3.3', '3.4.1', '3.5',
-                '4.0.2', '4.1', '4.2', '4.3', '4.4', '4.5'
+                '3.0', '3.1', '3.2.1', '3.3', '3.4.1', '3.5.1',
+                '4.0.2', '4.1', '4.2.1', '4.3.1', '4.4.1', '4.5'
         ]
     }
 
