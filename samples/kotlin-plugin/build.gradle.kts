2bbf8575a941baa45780b458b67e626a1295d8a3
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-03-01 12:11:19 +0000

    Update version properties for 1.1 release

--- samples/kotlin-plugin/build.gradle.kts
+++ samples/kotlin-plugin/build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.github.rodm:gradle-teamcity-plugin:1.1-rc-1")
+        classpath("com.github.rodm:gradle-teamcity-plugin:1.1")
     }
 }
 
