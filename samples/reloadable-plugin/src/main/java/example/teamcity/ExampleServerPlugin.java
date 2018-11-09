511c31ae8a38bea96d2ba37c90ac8dba21daf7c9
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-11-09 15:55:02 +0000

    Add sample to demonstrate plugin reloading without restarting the server

---
+++ samples/reloadable-plugin/src/main/java/example/teamcity/ExampleServerPlugin.java
@@ -0,0 +1,26 @@
+package example.teamcity;
+
+import jetbrains.buildServer.serverSide.BuildServerAdapter;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import org.apache.log4j.Logger;
+
+public class ExampleServerPlugin extends BuildServerAdapter {
+
+    private static Logger logger = Logger.getLogger("jetbrains.buildServer.SERVER");
+
+    private final SBuildServer server;
+
+    private final PluginDescriptor descriptor;
+
+    public ExampleServerPlugin(SBuildServer server, PluginDescriptor descriptor) {
+        this.server = server;
+        this.descriptor = descriptor;
+    }
+
+    public void register() {
+        server.addListener(this);
+        logger.info(String.format("ExampleServerPlugin: Plugin name: %s, version: %s", descriptor.getPluginName(), descriptor.getPluginVersion()));
+        logger.info(String.format("ExampleServerPlugin: Plugin parameter: param=%s", descriptor.getParameterValue("param")));
+    }
+}
