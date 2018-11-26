b40475e2de7d40db378805f8bd89d97f13723ede
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-11-26 16:25:04 +0000

    Update version for 1.2-beta-2 release

--- samples/kotlin-plugin/build.gradle.kts
+++ samples/kotlin-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     kotlin("jvm") version "1.2.0" apply false
-    id ("com.github.rodm.teamcity-server") version "1.2-beta-1" apply false
+    id ("com.github.rodm.teamcity-server") version "1.2-beta-2" apply false
 }
 
 group = "com.github.rodm.teamcity"
