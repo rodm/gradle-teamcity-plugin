91e8bfce52f4ae3efcf0614e9636d48b8fc3ef58
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2019-02-20 11:17:50 +0000

    Remove unused import

--- src/main/groovy/com/github/rodm/teamcity/PluginDeployment.groovy
+++ src/main/groovy/com/github/rodm/teamcity/PluginDeployment.groovy
@@ -22,8 +22,6 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.Optional
 
-import static groovy.transform.TypeCheckingMode.SKIP
-
 /**
  * Agent-side plugin deployment configuration
  */
