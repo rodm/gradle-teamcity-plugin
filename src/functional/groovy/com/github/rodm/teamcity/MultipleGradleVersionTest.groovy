9d6b01af8f9a4071fd8280c2f83e8b9077057355
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2019-08-26 19:51:04 +0100

    Test against Gradle 5.5.1 and 5.6

--- src/functional/groovy/com/github/rodm/teamcity/MultipleGradleVersionTest.groovy
+++ src/functional/groovy/com/github/rodm/teamcity/MultipleGradleVersionTest.groovy
@@ -54,7 +54,7 @@ class MultipleGradleVersionTest {
     @Parameterized.Parameters(name = 'Gradle {0}')
     static List<String> data() {
         return [
-            '5.0', '5.1.1', '5.2.1', '5.3.1', '5.4.1'
+            '5.0', '5.1.1', '5.2.1', '5.3.1', '5.4.1', '5.5.1', '5.6'
         ]
     }
 
