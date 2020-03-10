d7f9ec834e9bc627ee172f4cc80bddd7399217c4
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2020-03-10 11:37:44 +0000

    Test against Gradle 6.2.2

--- src/functional/groovy/com/github/rodm/teamcity/MultipleGradleVersionTest.groovy
+++ src/functional/groovy/com/github/rodm/teamcity/MultipleGradleVersionTest.groovy
@@ -55,7 +55,7 @@ class MultipleGradleVersionTest {
     static List<String> data() {
         return [
             '5.0', '5.1.1', '5.2.1', '5.3.1', '5.4.1', '5.5.1', '5.6.4',
-            '6.0.1', '6.1.1', '6.2'
+            '6.0.1', '6.1.1', '6.2.2'
         ]
     }
 
