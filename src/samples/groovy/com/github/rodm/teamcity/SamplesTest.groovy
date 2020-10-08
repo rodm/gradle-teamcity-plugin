c1135dfff7052a02d70e9887799c66ab7080e102
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2020-10-08 20:18:06 +0100

    Output warning messages

--- src/samples/groovy/com/github/rodm/teamcity/SamplesTest.groovy
+++ src/samples/groovy/com/github/rodm/teamcity/SamplesTest.groovy
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -91,9 +91,10 @@ class SamplesTest {
     }
 
     private static BuildResult executeBuild(File projectDir, String... args = ['clean', 'build']) {
+        def buildArgs = ['--warning-mode', 'all', *args]
         BuildResult result = GradleRunner.create()
                 .withProjectDir(projectDir)
-                .withArguments(args)
+                .withArguments(buildArgs)
                 .withPluginClasspath()
                 .forwardOutput()
                 .build()
