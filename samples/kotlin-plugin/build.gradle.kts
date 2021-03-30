f61ec3f785cf99877e5508907b0b01104a663b52
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2021-03-30 16:37:13 +0100

    Update example to use pluginManagement to control plugin version

--- samples/kotlin-plugin/build.gradle.kts
+++ samples/kotlin-plugin/build.gradle.kts
@@ -1,9 +1,4 @@
 
-plugins {
-    kotlin("jvm") version "1.3.72" apply false
-    id ("com.github.rodm.teamcity-server") version "1.4-beta-1" apply false
-}
-
 group = "com.github.rodm.teamcity"
 version = "1.0-SNAPSHOT"
 
--- samples/kotlin-plugin/settings.gradle.kts
+++ samples/kotlin-plugin/settings.gradle.kts
@@ -1,4 +1,11 @@
 
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "1.3.72"
+        id("com.github.rodm.teamcity-server") version "1.4-beta-1"
+    }
+}
+
 rootProject.name = "kotlin-example-plugin"
 
 include("common")
